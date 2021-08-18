8c9a58d0053c79dd925ad51f3d60cab136908cae
Sherman Chen <schen@simviu.com>
2021-08-18 18:25:50 +0800

    adj para

--- tools/run_train2.sh
+++ tools/run_train2.sh
@@ -1,3 +1,3 @@
-python train_rcnn.py --cfg_file cfgs/default.yaml --batch_size 16 --train_mode rpn --epochs 200
+python train_rcnn.py --cfg_file cfgs/default.yaml --batch_size 8 --train_mode rpn --epochs 100
 
 
