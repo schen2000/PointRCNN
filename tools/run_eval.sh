dde3b400be5b1f59ea1e8f31d4ef73ef6b26da9d
Sherman Chen <schen@simviu.com>
2021-09-18 15:08:54 +0800

    run_eval works

--- tools/run_eval.sh
+++ tools/run_eval.sh
@@ -1,3 +1,3 @@
-CKPT=./ckpt/checkpoint_epoch_95.pth
+CKPT=../data/wdir/pre-trained/PointRCNN.pth
 python eval_rcnn.py --cfg_file cfgs/default.yaml --ckpt $CKPT --batch_size 1 --eval_mode rcnn --set RPN.LOC_XZ_FINE False
 
